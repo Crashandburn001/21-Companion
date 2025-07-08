643a5a41083640816bedb21a58b028ce097fe482
Crashandburn001 <lwhit503@eq.edu.au>
2025-07-08 10:33:56 +1000

    Added main menu.

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 import colorama
 from colorama import Fore, Back, Style
-
+menuChoice = 0
 def menu():
-    print(Fore.RED + "Welcome to 21-Companion!!!")
+    global menuChoice
+    print(Fore.BLUE + Style.BRIGHT + "Welcome to 21-Companion!!!")
+    while menuChoice == 0 or menuChoice == '!':
+        try:
+            print("\n" + Fore.CYAN + Style.BRIGHT + "Menu")
+            print(Style.RESET_ALL + "\n\n" + Fore.GREEN + "[1] Play\n" + Fore.LIGHTMAGENTA_EX + "[2] Exit Game\n" + Fore.YELLOW + "[3] Credits")
+            menuChoice = int(input(Fore.WHITE + Style.BRIGHT + "\nEnter your choice: "))
+        except ValueError:
+            print(Fore.RED + Style.NORMAL + "Uh Oh! Looks like you didn't enter a number. Please try again and set your choice to the number next to the menu option!")
+            menuChoice = '!'
+        if menuChoice == 1:
+            play()
+        elif menuChoice == 2:
+            rusure = input(Fore.RED + Style.NORMAL + "Are you sure you wish to exit the game? (y/n) \t")
+            if rusure == "y":
+                exit()
+            elif rusure == "n":
+                menuChoice = 0
+            else:
+                print("Invalid Input. Returning to menu.")
+        elif menuChoice == 3:
+            print(Style.RESET_ALL + "This game was created by " + Style.BRIGHT + Fore.BLUE + "Crashandburn001." + Style.RESET_ALL + "\n It is designed to help players of the game " + Style.BRIGHT + Fore.BLUE + "21/Blackjack" + Style.RESET_ALL + " with game administration!")
+            menuChoice = 0
+        elif menuChoice != '!':
+            print(Style.RESET_ALL + Fore.RED + "Invalid Input. Returning to menu.")
+            menuChoice = 0
+        else:
+            menuChoice = 0
 
+
+def play():
+    print("Play!")
+
+
+# Execute Functions!
 menu()
